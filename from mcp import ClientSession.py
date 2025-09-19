212cc04176b3cbfccd4c7e0c1c754870241a9bef
Ashvini <125997432+ashvinibalte@users.noreply.github.com>
2025-09-19 11:50:23 -0400

    Update from mcp import ClientSession.py

--- from mcp import ClientSession.py
+++ from mcp import ClientSession.py
@@ -1,1 +1,16 @@
+https://www.mongodb.com/docs/languages/python/pymongo-driver/current/get-started/
+
+https://gofastmcp.com/servers/prompts
+
+https://gofastmcp.com/servers/tools
+
+https://github.com/jlowin/fastmcp
+
+https://www.mongodb.com/docs/languages/python/pymongo-driver/current/get-started/
+
+https://pymongo.readthedocs.io/en/stable/api/pymongo/collection.html
+
+https://codesignal.com/learn/courses/developing-and-integrating-a-mcp-server-in-python/lessons/exploring-and-exposing-mcp-server-capabilities-tools-resources-and-prompts
+
+
 
