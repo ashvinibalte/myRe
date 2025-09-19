7f71c0d79be1714b3f31e4aa40ef6ac8fce10807
Ashvini <125997432+ashvinibalte@users.noreply.github.com>
2025-09-18 21:30:21 -0400

    Update # Query a collection.py

--- # Query a collection.py
+++ # Query a collection.py
@@ -1,17 +1,2 @@
 # Query a collection
-record = await session.call_tool(
-    "find_by_userid",
-    {
-        "db_name": "modq",
-        "collection_name": "crm_processed_records",
-        "user_id": "ebb08033-10bd-454d-833c-840199e013a9"
-    }
-)
-print("Record:", record)
 
-# Get recent documents
-records = await session.call_tool(
-    "list_recent",
-    {"db_name": "modq", "collection_name": "crm_processed_records", "limit": 3}
-)
-print("Recent Records:", records)
