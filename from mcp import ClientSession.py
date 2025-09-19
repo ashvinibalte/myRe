7166a4ce0d7169b0baca5ac9d7d7ee1f5d975725
Ashvini <125997432+ashvinibalte@users.noreply.github.com>
2025-09-19 10:56:39 -0400

    Update from mcp import ClientSession.py

--- from mcp import ClientSession.py
+++ from mcp import ClientSession.py
@@ -1,9 +1,1 @@
-call_tool list_recent {"db_name":"modq","collection_name":"crm_processed_records","limit":5}
-
-
-call_tool find_many {"db_name":"modq","collection_name":"crm_processed_records","filter":{"Type":"AccountInsight"},"sort":[["UpdatedTimestamp",-1]],"limit":3}
-
-
-call_tool find_one {"db_name":"modq","collection_name":"crm_processed_records","filter":{"_id":"6689fa01d4f3f9cbeaa0a123"}}
-
 
